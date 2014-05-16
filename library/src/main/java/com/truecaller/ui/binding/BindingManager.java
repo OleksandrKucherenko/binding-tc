da475b61b9528338ed925a99f996004ad3c8a4c9
oleksandr <oleksandr.kucherenko@truecaller.com>
2014-05-16 13:01:12 +0200

    CHG: travis requested for emulator running ADD: first empty class declaration CHG: more comments added into GRADLE scripts, explain all the settings in details

---
+++ library/src/main/java/com/truecaller/ui/binding/BindingManager.java
@@ -0,0 +1,11 @@
+package com.truecaller.ui.binding;
+
+/** Manager class responsible for controlling integration of the binding library into corresponding fragment or
+ * activity. It controls aspects:<br/>
+ * <ul>
+ *     <li>context instance extracting;</li>
+ *     <li>Binding defining and configuring;</li>
+ *     <li></li>
+ * </ul>*/
+public class BindingManager {
+}
