a550e80e1d1927decf869f8b7fd6a82901addd29
Oleksandr Kucherenko <kucherenko.alex@gmail.com>
2015-08-08 17:32:18 +0200

    FIX: coveralls integration

--- gradle/custom/emulate-travis-pro.cmd
+++ gradle/custom/emulate-travis-pro.cmd
@@ -9,14 +9,14 @@ cd ..\..
 set TRAVIS=true
 set TRAVIS_JOB_ID=54078341
 set SERVICE_NAME=travis-ci
-set COVERALLS_REPO_TOKEN=eMlGHyXPJPqm0sn91H2HwOls3mSIrz15C
+set COVERALLS_REPO_TOKEN=7mBDEXUP7nzDjqza6ZRjakgIvy10aXQDt
 
-set CI_BUILD_NUMBER=104
-set CI_BUILD_URL=https://travis-ci.org/OleksandrKucherenko/meter/builds/54078340
+set CI_BUILD_NUMBER=42
+set CI_BUILD_URL=https://travis-ci.org/OleksandrKucherenko/binding-tc/builds/74676002
 ::set CI_NAME=
 
 ::gradlew coveralls --info %1 %2 %3 %4 %5 %6 %7
-gradlew :meter:coveralls --stacktrace --debug 2>&1 | tee debug.log
+gradlew :binder:coveralls --stacktrace --info 2>&1 | tee info.log
 
 :: recover directory
 popd
