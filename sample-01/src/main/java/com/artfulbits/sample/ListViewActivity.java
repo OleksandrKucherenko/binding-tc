9c2d4fb8d6d1b111b77b2650ddb0c6f0520ce017
oleksandr <kucherenko.alex@gmail.com>
2015-08-14 09:12:54 +0200

    ADD: stabs for future development

---
+++ sample-01/src/main/java/com/artfulbits/sample/ListViewActivity.java
@@ -0,0 +1,7 @@
+package com.artfulbits.sample;
+
+import android.app.ListActivity;
+
+/** Typical usage of List Activity. */
+public class ListViewActivity extends ListActivity {
+}
--- sample-01/src/main/java/com/artfulbits/sample/ListViewFragment.java
+++ sample-01/src/main/java/com/artfulbits/sample/ListViewFragment.java
@@ -2,6 +2,6 @@ package com.artfulbits.sample;
 
 import android.support.v4.app.ListFragment;
 
-/** */
+/** Typical use of ListFragment. */
 public class ListViewFragment extends ListFragment {
 }
